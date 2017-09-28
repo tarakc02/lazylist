4d9cb20d50dd70e4b9a2867e4a181837d8b03628
tarakc02 <tarakc02@github.com>
2017-09-28 09:34:57 -0700

    don't fill up the call stack when filtering streams -- use a while loop

--- R/filter.R
+++ R/filter.R
@@ -1,5 +1,5 @@
 #' Filter a stream using a predicate function
-#' 
+#'
 #' @param stream The stream to be filtered
 #' @param predicate A function that takes a stream element as an argument, and returns TRUE/FALSE
 #' @export
@@ -7,7 +7,7 @@ stream_filter <- function(stream, predicate) UseMethod("stream_filter")
 
 #' @export
 stream_filter.lazylist <- function(stream, predicate) {
-    if (predicate(stream_car(stream)))
-        cons_stream(stream_car(stream), stream_filter(stream_cdr(stream), predicate))
-    else stream_filter(stream_cdr(stream), predicate)
+    while(!predicate(stream_car(stream))) stream <- stream_cdr(stream)
+    cons_stream(stream_car(stream),
+                stream_filter(stream_cdr(stream), predicate))
 }
--- R/stream_at.R
+++ R/stream_at.R
@@ -1,18 +1,11 @@
 #' Select elements of a stream
-#' 
+#'
 #' @param stream A stream to select from
 #' @param filter A stream of TRUE/FALSE values
 #' @export
 stream_at <- function(stream, filter) {
-    if (stream_car(filter))
-        return(cons_stream(
-            stream_car(stream),
-            stream_at(stream_cdr(stream),
-                      stream_cdr(filter))
-        ))
-    else
-        stream_at(
-            stream_cdr(stream),
-            stream_cdr(filter)
-        )
+    while (!stream_car(filter)) stream <- stream_cdr(stream)
+    cons_stream(stream_car(stream),
+                stream_at(stream_cdr(stream),
+                          stream_cdr(filter)))
 }
