d6ab05c9b15e0275eb8b8e6a85e0e3e3ff97fc0a
tarakc02 <tarakc02@users.noreply.github.com>
2017-10-02 18:00:37 -0700

    ability to merge finite streams

--- R/merge_weighted.R
+++ R/merge_weighted.R
@@ -1,14 +1,14 @@
 #' Merge two streams so that the result is ordered
-#' 
+#'
 #' The weight function is used to order the elements
-#' 
+#'
 #' @param stream1 a lazy list
 #' @param stream2 a lazy list
 #' @param weight a function with one required argument
 #' @export
 merge_weighted <- function(stream1, stream2, weight) {
-    if (is.null(stream1)) stream2
-    if (is.null(stream2)) stream1
+    if (is.null(stream1)) return(stream2)
+    if (is.null(stream2)) return(stream1)
 
     s1car <- stream_car(stream1)
     s2car <- stream_car(stream2)
